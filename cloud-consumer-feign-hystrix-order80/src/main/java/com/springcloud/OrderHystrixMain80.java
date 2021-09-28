440566756adb05eab0d6df56861dbe8a7ec6edb8
dxf <237881603@qq.com>
2021-09-28 17:15:11 +0800

    Hystrix学习

---
+++ cloud-consumer-feign-hystrix-order80/src/main/java/com/springcloud/OrderHystrixMain80.java
@@ -0,0 +1,22 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * @description： TODO
+ * @author: DXF
+ * @date: 2021-09-28 14:37:10
+ * @version: 1.0
+ */
+
+@SpringBootApplication
+@EnableFeignClients
+@EnableHystrix
+public class OrderHystrixMain80 {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderHystrixMain80.class, args);
+    }
+}
