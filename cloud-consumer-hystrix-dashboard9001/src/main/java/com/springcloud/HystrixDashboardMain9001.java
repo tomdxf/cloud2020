6328b446e57b49ee72adbef28fb354ec3e9104e3
dxf <237881603@qq.com>
2021-09-29 16:32:32 +0800

    Hystrix学习

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,19 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @description： TODO
+ * @author: DXF
+ * @date: 2021-09-29 16:15:12
+ * @version: 1.0
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
