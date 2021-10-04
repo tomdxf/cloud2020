527aa2bd5e960aef48185e238fde51e1b4a7bc7b
dxf <237881603@qq.com>
2021-10-04 16:23:08 +0800

    Config学习

---
+++ cloud-stream-rabbitmq-consumer8802/src/main/java/com/springcloud/StreamConsumerMain8802.java
@@ -0,0 +1,17 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description： 主启动
+ * @author: DXF
+ * @date: 2021-10-04 16:04:31
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class StreamConsumerMain8802 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamConsumerMain8802.class,args);
+    }
+}
---
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/springcloud/StreamProviderMain8801.java
@@ -0,0 +1,17 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description： 主启动
+ * @author: DXF
+ * @date: 2021-10-04 16:02:24
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class StreamProviderMain8801 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamProviderMain8801.class,args);
+    }
+}
