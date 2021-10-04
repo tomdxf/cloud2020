a8ccaa84c8caea7b63a64d52e7284a0ef0018b67
dxf <237881603@qq.com>
2021-10-04 17:05:07 +0800

    Stream学习

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/springcloud/StreamConsumerMain8803.java
@@ -0,0 +1,17 @@
+package com.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description： TODO
+ * @author: DXF
+ * @date: 2021-10-04 16:29:33
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class StreamConsumerMain8803 {
+    public static void main(String[] args) {
+        SpringApplication.run(StreamConsumerMain8803.class,args);
+    }
+}
--- cloud-stream-rabbitmq-provider8801/src/main/java/com/springcloud/service/IMessageProviderImpl.java
+++ cloud-stream-rabbitmq-provider8801/src/main/java/com/springcloud/service/IMessageProviderImpl.java
@@ -19,6 +19,6 @@ public class IMessageProviderImpl implements IMessageProvider {
         String serial = UUID.randomUUID().toString();
         output.send(MessageBuilder.withPayload(serial).build());
         System.out.println("******serial: " + serial);
-        return null;
+        return serial;
     }
 }
